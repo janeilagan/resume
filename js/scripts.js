7b22036a896f0ad67cb7e9b1f65ed0a0dfac4334
Jane Ann Ilagan <janeannilagan@outlook.com>
2023-02-07 18:25:41 +0800

    update on image hovers

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,34 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+// Image hover script
+
+let attached = false;
+
+const getElmtImage = (elmt) => {
+  return elmt.querySelector("img")
+}
+
+const followMouse = (elmt, event) => {
+  elmt.style.left = event.x + "px";
+  elmt.style.top = event.y + "px";
+}
+
+function showImage(elmt) {
+  const image = getElmtImage(elmt)
+  if (!attached) {
+    attached = true;
+    image.style.display = "block";
+    document.addEventListener("pointermove", function(event) {
+      followMouse(image, event)
+    });
+  }
+}
+
+function hideImage(elmt) {
+  const image = getElmtImage(elmt)
+  attached = false;
+  image.style.display = "none";
+  document.removeEventListener("pointermove", followMouse);
+}
